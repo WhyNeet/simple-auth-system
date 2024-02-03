d4d1f6d02cc9187f4df8bdf4bd01744530785c6a
ArcSoftwareX <arc.software.x@gmail.com>
2024-02-03 18:26:36 +0300

    chore: add ApiError class for response errors

---
+++ src/main/java/dev/whyneet/authsystem/api/exception/ApiError.java
@@ -0,0 +1,36 @@
+package dev.whyneet.authsystem.api.exception;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public class ApiError {
+    private String message;
+    private List<String> error;
+
+    public ApiError(String message, String error) {
+        this.message = message;
+        this.error = List.of(error);
+    }
+
+    public ApiError(String message, List<String> error) {
+        this.message = message;
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<String> getError() {
+        return error;
+    }
+
+    public void setError(List<String> error) {
+        this.error = error;
+    }
+}
