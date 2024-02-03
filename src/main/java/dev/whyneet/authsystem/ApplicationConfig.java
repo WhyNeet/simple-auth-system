c7fe75d55a8a3fc71142e2cd7209148050940a06
ArcSoftwareX <arc.software.x@gmail.com>
2024-02-03 17:20:42 +0300

    chore: add ApplicationConfig

---
+++ src/main/java/dev/whyneet/authsystem/ApplicationConfig.java
@@ -0,0 +1,32 @@
+package dev.whyneet.authsystem;
+
+import dev.whyneet.authsystem.api.security.UserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class ApplicationConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new Argon2PasswordEncoder(64, 128, 2, 60000, 10);
+    }
+}
